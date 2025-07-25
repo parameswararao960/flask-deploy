74679e23a3e1c86be5901d342f49c33fbfb302ae
parameswararao960 <parama960@gmail.com>
2025-07-25 19:12:05 +0530

    Added db

--- app.py
+++ app.py
@@ -1,10 +1,38 @@
 from flask import Flask
+from sqlalchemy import create_engine, Column, Integer, String
+from sqlalchemy.orm import declarative_base, sessionmaker
 
 app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///myDataBase.db'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+
+engine = create_engine(app.config['SQLALCHEMY_DATABASE_URI'])
+Session = sessionmaker(bind=engine)
+session = Session()
+Base = declarative_base()
+
+class User(Base):
+    __tablename__ = 'users'
+    id = Column(Integer, primary_key=True)
+    name = Column(String, nullable=False)
+    age = Column(Integer, nullable=False)
+
+def init_db():
+    Base.metadata.create_all(bind=engine)
+    session = Session()
+    if not session.query(User).first():
+        session.add(User(name="Admin", age=25))
+        session.commit()
+    session.close()
+
+with app.app_context():
+    init_db()
+
 
 @app.route('/')
 def home():
-    return "Hello, World!"
+    users = session.query(User).all()
+    return f"Users in DB: {[(u.name, u.age) for u in users]}"
 
 if __name__ == "__main__":
     app.run()
